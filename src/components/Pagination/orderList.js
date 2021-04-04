551bff2c986965aec9792894a8f782956734cad7
AntonioDeveloper <juniorgabrielsantana@hotmail.com>
2021-04-04 06:18:54 -0300

    Teste

--- src/components/Pagination/orderList.js
+++ src/components/Pagination/orderList.js
@@ -10,7 +10,7 @@ export default function OrderList({orders}){
     i++;
     return i;
      });  */
-     console.log(typeof(orders));
+     console.log(orders);
   return(
     <Table striped responsive>
       <thead>
